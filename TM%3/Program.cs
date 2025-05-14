00729b7311ecee769408d42cd5018a80aac3922f
mohammedabdelaleem <mohammedabdelaleem20@gmail.com>
2025-05-14 22:04:07 +0300

    Turing Machine that recognizes the language L= { Accept binary numbers divisible by 3}

---
+++ TM%3/Program.cs
@@ -0,0 +1,29 @@
+﻿using TM_3;
+
+char again = 'n';
+
+do
+{
+	try
+	{
+		Console.Clear();
+
+		Console.Write("Enter a binary number:");
+		string input = Console.ReadLine();
+
+		int decimalValue = Utils.BinaryToDecimal(input);
+		bool isDivisible = Utils.IsDivisibleBy3(input);
+
+		Console.Write($"Decimal value: {decimalValue}\n");
+		Console.WriteLine(isDivisible
+			? " Accepted: Number is divisible by 3.\n"
+			: " Rejected: Number is not divisible by 3.\n");
+	}
+	catch (ArgumentException ex)
+	{
+		Console.WriteLine("Error: " + ex.Message);
+	}
+
+	Console.Write("Try Again? [Y | N] : ");
+	again = char.Parse(Console.ReadLine().Substring(0, 1));
+} while (char.ToUpper(again) == 'Y');
---
+++ TM%3/Utils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TM_3;
+public static class Utils
+{
+	enum State { Q0, Q1, Q2 }
+
+	public static bool IsDivisibleBy3(string binary)
+	{
+		State currentState = State.Q0;
+
+		foreach (char symbol in binary)
+		{
+			if (symbol != '0' && symbol != '1')
+			{
+				throw new ArgumentException("Invalid binary digit: " + symbol);
+			}
+
+			switch (currentState)
+			{
+				case State.Q0:
+					currentState = (symbol == '0') ? State.Q0 : State.Q1;
+					break;
+				case State.Q1:
+					currentState = (symbol == '0') ? State.Q2 : State.Q0;
+					break;
+				case State.Q2:
+					currentState = (symbol == '0') ? State.Q1 : State.Q2;
+					break;
+			}
+		}
+
+		return currentState == State.Q0; // Accept if in state Q0
+	}
+
+	public static int BinaryToDecimal(string binary)
+	{
+		if (string.IsNullOrEmpty(binary) || !binary.All(c => c == '0' || c == '1'))
+			throw new ArgumentException("Input must be a binary string (only 0s and 1s).");
+
+		return Convert.ToInt32(binary, 2);
+	}
+}
